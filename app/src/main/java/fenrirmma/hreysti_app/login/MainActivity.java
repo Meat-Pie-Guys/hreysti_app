032f1cf38cf1986ca0d2bfd54838cea2820e71bc
JohannIngi <johannb16@ru.is>
2017-12-08 11:01:12 +0000

    login and signup ready

--- app/src/main/java/fenrirmma/hreysti_app/login/MainActivity.java
+++ app/src/main/java/fenrirmma/hreysti_app/login/MainActivity.java
@@ -3,21 +3,28 @@ package fenrirmma.hreysti_app.login;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.koushikdutta.ion.Ion;
+
+import java.util.Objects;
+
 import fenrirmma.hreysti_app.R;
+import fenrirmma.hreysti_app.user.adminActivity;
 import fenrirmma.hreysti_app.user.clientActivity;
+import fenrirmma.hreysti_app.user.coachActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private SessionAccess sa;
-    private EditText username;
+    private EditText ssn;
     private EditText password;
-    private RelativeLayout load;
-    private String role;
+    //private RelativeLayout load;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        username = findViewById(R.id.et_username);
-        password = findViewById(R.id.et_password);
+        ssn = findViewById(R.id.sign_in_ssn);
+        password = findViewById(R.id.sign_in_pw);
         sa = SessionAccess.getInstance(this);
 
         if(sa.hasToken()) proceed();
@@ -35,9 +42,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void signIn(View view){
 
-        String un = username.getText().toString();
+        String _ssn = ssn.getText().toString();
         String pw = password.getText().toString();
+        getToken(_ssn, pw);
 
+    }
+
+    private void getToken(String ssn, String pw) {
+        String credentials = new String(Base64.encode(String.format("%s:%s", ssn, pw).getBytes(), Base64.DEFAULT));
+        Ion.with(this)
+                .load("GET", "http://10.0.2.2:5000/login")
+                .addHeader("Authorization", String.format("Basic %s", credentials))
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Accept", "application/json")
+                .setTimeout(1000)
+                .asJsonObject()
+                .setCallback((e, result) -> {
+                    if(e != null){
+                        Toast.makeText(this, "Ion error", Toast.LENGTH_SHORT).show(); //TODO breyta í eitthvað meira hot
+                    }
+                    else {
+                        String code = result.get("error").getAsString();
+                        if (!Objects.equals(code, "0")) {
+                            Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show(); //TODO breyta í eitthvað meira hot
+                        }
+                        else{
+                            sa.setRole(result.get("role").getAsString());
+                            sa.setToken(result.get("token").getAsString());
+                            proceed();
+                        }
+                    }
+                });
     }
 
     public void signUp(View view){
@@ -47,13 +82,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void proceed() {
-        startActivity(new Intent(this, clientActivity.class));
-        finish();
+        String role = sa.getRole();
+        if(role.equals("Admin")){
+            startActivity(new Intent(this, adminActivity.class));
+            finish();
+        } else if(role.equals("Coach")){
+            startActivity(new Intent(this, coachActivity.class));
+            finish();
+        } else{
+            startActivity(new Intent(this, clientActivity.class));
+            finish();
+        }
 
     }
 
-    private void toast(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
 }
 
