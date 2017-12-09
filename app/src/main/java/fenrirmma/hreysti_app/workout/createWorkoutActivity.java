89e8e0663a1b0c19cb1c85e4b1746d3bcb8ef5cc
JohannIngi <johannb16@ru.is>
2017-12-09 16:07:47 +0000

    can create a workout description and it adds to the db

--- app/src/main/java/fenrirmma/hreysti_app/workout/createWorkoutActivity.java
+++ app/src/main/java/fenrirmma/hreysti_app/workout/createWorkoutActivity.java
@@ -1,15 +1,62 @@
 package fenrirmma.hreysti_app.workout;
 
+import android.os.TestLooperManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.ion.Ion;
 
 import fenrirmma.hreysti_app.R;
+import fenrirmma.hreysti_app.login.SessionAccess;
 
 public class createWorkoutActivity extends AppCompatActivity {
+
+    private SessionAccess sa;
+    private EditText workout, type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+
+        sa = SessionAccess.getInstance(this);
+        workout = findViewById(R.id.workout);
+        type = findViewById(R.id.set_type_text);
+
+    }
+
+    public void submitWorkout(View view) {
+
+        JsonObject json = new JsonObject();
+        json.addProperty("text", workout.getText().toString());
+        json.addProperty("type", type.getText().toString());
+        Ion.with(this)
+                .load("POST", "http://10.0.2.2:5000/description")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Accept", "application/json")
+                .addHeader("fenrir-token", sa.getToken())
+                .setTimeout(1000)
+                .setJsonObjectBody(json)
+                .asJsonObject()
+                .setCallback((e, result) -> {
+                    if(e != null){
+                        Toast.makeText(this, "ION ERROR", Toast.LENGTH_SHORT).show(); //TODO breyta í eitthvað meira hot
+                    }else {
+                        int code = result.get("error").getAsInt();
+                        if (code != 0) {
+                            Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show(); //TODO breyta í eitthvað meira hot
+                        }
+                        else{
+                            Toast.makeText(this, "SUCCESS", Toast.LENGTH_SHORT).show();
+                            // TODO Eitthvað success skilabð betra en Toastið
+                        }
+                    }
+                });
+        finish();
     }
 }
