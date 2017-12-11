bb44efde3d30a8f3f6592dd0841287b512578fcc
Magnús Torfi Magnússon <magnustorfi@gmail.com>
2017-12-11 19:06:16 +0000

    recycler adapter for admin in progress

--- app/src/main/java/fenrirmma/hreysti_app/user/Admin/AllUsersAdapter.java
+++
@@ -1,9 +0,0 @@
-package fenrirmma.hreysti_app.user.Admin;
-
-/**
- * Created by magnu on 10/12/2017.
- */
-
-public class AllUsersAdapter {
-
-}
---
+++ app/src/main/java/fenrirmma/hreysti_app/user/Admin/AllUsersRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package fenrirmma.hreysti_app.user.Admin;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fenrirmma.hreysti_app.R;
+
+/**
+ * Created by magnu on 10/12/2017.
+ */
+
+public class AllUsersRecyclerAdapter extends RecyclerView.Adapter<AllUsersRecyclerAdapter.ViewHolder> {
+
+    private List<UserHelper> userList;
+    private Activity activity;
+
+    private AllUsersRecyclerAdapter(Activity activity, List<UserHelper> userList){
+        this.userList = userList;
+        this.activity = activity;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType){
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_get_users_single_item, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(AllUsersRecyclerAdapter.ViewHolder viewHolder, int pos){
+        viewHolder.name.setText(userList.get(pos).getName());
+        viewHolder.role.setText(userList.get(pos).getUserRole());
+        viewHolder.ssn.setText(userList.get(pos).getSsn());
+        viewHolder.expiration.setText(userList.get(pos).getExpireDate());
+    }
+
+    @Override
+    public int getItemCount(){return(null != userList ? userList.size() : 0);}
+
+    protected class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView image;
+        private TextView name;
+        private TextView role;
+        private TextView ssn;
+        private TextView expiration;
+        private View container;
+
+        public ViewHolder(View view){
+            super(view);
+            image = view.findViewById(R.id.image);
+            name = view.findViewById(R.id.name);
+            role = view.findViewById(R.id.role);
+            ssn = view.findViewById(R.id.ssn);
+            expiration = view.findViewById(R.id.expiration);
+            container = view.findViewById(R.id.card_view);
+        }
+    }
+}
+
